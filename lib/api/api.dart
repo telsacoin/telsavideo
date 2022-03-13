d1e0dd5bb465b0276e9e9e35baf1fa61a0b11481
TSLA Coin <6106454+TelsaV@users.noreply.github.com>
2022-03-13 13:32:12 +0800

    update file name

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,22 +3,46 @@ import 'package:telsavideo/api/http_manager.dart';
 import 'package:telsavideo/constants.dart';
 import 'package:telsavideo/http/http_constant.dart';
 import 'package:telsavideo/models/dto/recommend/itemlist_dto.dart';
+import 'package:telsavideo/models/dto/signin/signin_dto.dart';
 import 'package:telsavideo/models/vo/recommend/itemlist_vo.dart';
+import 'package:telsavideo/models/vo/signin/signin_vo.dart';
 
 class Api {
   /// the app interface
   static String api = apiPortail;
-  static Future<ItemListVo> getRecommendItemList(ItemListDto? dto) async {
-    Map<String, dynamic> map = new HashMap();
-    map["cursor"] = dto?.cursor ?? 1;
-    map["count"] = dto?.count ?? 10;
-    var result = await HttpManager.getInstance()
-        .get(
-            url: HttpConstant.recommendItemList,
-            cancelTokenTag: 'recomment',
-            param: map)
-        .then((value) => ItemListVo.fromJson(value));
 
-    return result;
+  ///signin the app
+  static Future<SignInVo> getLoginResponse(SignInDto? dto) async {
+    Map<String, dynamic> params = new HashMap();
+    params["username"] = dto?.username;
+    params["password"] = dto?.password;
+    var result = await HttpManager.getInstance()
+        .post(url: HttpConstant.login, cancelTokenTag: 'signin', data: params);
+    return SignInVo.fromJson(result);
+  }
+
+  /// get recommend item list
+  static Future<ItemListVo> getRecommendItemList(ItemListDto? dto) async {
+    Map<String, dynamic> params = new HashMap();
+    params["cursor"] = dto?.cursor ?? 0;
+    params["count"] = dto?.count ?? 20;
+    var result = await HttpManager.getInstance().get(
+        url: HttpConstant.recommendItemList,
+        cancelTokenTag: 'recomment',
+        param: params);
+
+    return ItemListVo.fromJson(result);
+  }
+
+  /// the following item list videos
+  static Future<ItemListVo> getFollowingItemList(ItemListDto? dto) async {
+    Map<String, dynamic> params = new HashMap();
+    params["cursor"] = dto?.cursor ?? 0;
+    params["count"] = dto?.count ?? 20;
+    var result = await HttpManager.getInstance().get(
+        url: HttpConstant.followingItemList,
+        cancelTokenTag: 'following',
+        param: params);
+    return ItemListVo.fromJson(result);
   }
 }
--- lib/models/vo/signin/signin_vo.dart
+++ lib/models/vo/signin/signin_vo.dart
@@ -1,3 +1,6 @@
+/// the telsacoin
+/// author:cw
+
 class SignInVo {
   String? accessToken;
   int? expiresIn;
@@ -7,4 +10,30 @@ class SignInVo {
   String? token;
   String? userInfo;
   String? userName;
+
+  SignInVo({
+    this.accessToken,
+    this.expiresIn,
+    this.expiresInDate,
+    this.permitToken,
+    this.refreshToken,
+    this.token,
+    this.userInfo,
+    this.userName,
+  });
+
+  factory SignInVo.fromJson(Map<String, dynamic> json) => SignInVo(
+        accessToken: json["accessToken"] == null ? null : json["accessToken"],
+        expiresIn: json["expiresIn"] == null ? null : json["expiresIn"],
+        expiresInDate:
+            json["expiresInDate"] == null ? null : json["expiresInDate"],
+        permitToken: json["permitToken"] == null ? null : json["permitToken"],
+        refreshToken:
+            json["refreshToken"] == null ? null : json["refreshToken"],
+        token: json["token"] == null ? null : json["token"],
+        userInfo: json["userInfo"] == null ? null : json["userInfo"],
+        userName: json["userName"] == null ? null : json["userName"],
+      );
+
+  Map<String, dynamic> toJson() => {};
 }
